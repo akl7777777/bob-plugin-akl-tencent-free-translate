d8dfaa6c1c2fd1051203d9bbaeccff2ef53ed276
akl7777777 <84266551+akl7777777@users.noreply.github.com>
2023-02-27 15:23:18 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# bob-plugin-akl-tencent-translate
+
+
+下载地址:[腾讯翻译君(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-tencent-translate/releases/download/v_0.0.1/bob-plugin-akl-tencent-translate_v0.0.1.bobplugin)
+
+### bob翻译插件大合集:
+
+>[DeepL翻译插件(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-deepl-free-translate)
+
+>[有道翻译插件(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-youdao-free-translate)
+
+>[百度翻译插件(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-baidu-free-translate)
+
+>[彩云小译插件(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-caiyunxiaoyi-free-translate)
+
+>[只为日语 - MOJi辞書（じしょ）](https://github.com/akl7777777/bob-plugin-akl-mojidict-translate)
+
+
+
+### 开发不易,如果喜欢可以请作者喝一杯可乐,谢谢!
+
+
+
+
+![image](https://user-images.githubusercontent.com/84266551/219829283-3ed1798e-aeed-4174-bbcb-f93bf3008817.png)
---
+++ src/main.js
@@ -0,0 +1,168 @@
+var config = require('./config.js');
+var utils = require('./utils.js');
+var CryptoJS = require("crypto-js");
+
+// 加密
+function btoa(str) {
+    return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(str));
+}
+
+// 解密
+function atob(str) {
+    return CryptoJS.enc.Base64.parse(str).toString(CryptoJS.enc.Utf8);
+}
+
+
+function supportLanguages() {
+    return config.supportedLanguages.map(([standardLang]) => standardLang);
+}
+
+async function login() {
+    try {
+        return await $http.request({
+            method: "POST",
+            url: "https://fanyi.qq.com/api/reauth12f",
+            header: {},
+            body: {}
+        });
+    } catch (e) {
+        $log.error('接口请求错误 ==> ' + JSON.stringify(e))
+        Object.assign(e, {
+            _type: 'network',
+            _message: '接口请求错误 - ' + JSON.stringify(e),
+        });
+        throw e;
+    }
+}
+
+async function _translate(source, target, sourceText, qtv, qtk) {
+    const sessionUuid = 'translate_uuid' + new Date().getTime()
+    $log.error('接口请求错误 参数 ==> ' + JSON.stringify({
+        "source": source,
+        "target": target,
+        "sourceText": sourceText,
+        "qtv": qtv,
+        "qtk": qtk,
+        "sessionUuid": sessionUuid
+    }))
+
+    try {
+
+        return await $http.request({
+            method: "POST",
+            url: "https://fanyi.qq.com/api/translate",
+            header: {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
+            body: {
+                "source": source,
+                "target": target,
+                "sourceText": sourceText,
+                "qtv": qtv,
+                "qtk": qtk,
+                "sessionUuid": sessionUuid
+            }
+        });
+    } catch (e) {
+        $log.error('接口请求错误 ==> ' + JSON.stringify(e))
+        Object.assign(e, {
+            _type: 'network',
+            _message: '接口请求错误 - ' + JSON.stringify(e),
+        });
+        throw e;
+    }
+}
+
+async function __translate(source, target, sourceText) {
+    try {
+        return await $http.request({
+            method: "GET",
+            url: "https://wxapp.translator.qq.com/api/translate",
+            header: {
+                "content-type": "application/json",
+                "Host": "wxapp.translator.qq.com",
+                "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.32(0x18002035) NetType/WIFI Language/zh_TW",
+                "Referer": "https://servicewechat.com/wxb1070eabc6f9107e/117/page-frame.html"
+            },
+            body: {
+                "source": source,
+                "target": target,
+                "sourceText": sourceText,
+                "platform": "WeChat_APP",
+                "candidateLangs": 'zh|en',
+                "guid": "oqdgX0SIwhvM0TmqzTHghWBvfk22"
+            }
+        });
+    } catch (e) {
+        $log.error('接口请求错误 ==> ' + JSON.stringify(e))
+        Object.assign(e, {
+            _type: 'network',
+            _message: '接口请求错误 - ' + JSON.stringify(e),
+        });
+        throw e;
+    }
+}
+
+function readFile(filePath) {
+    if ($file.exists(filePath)) {
+        return $file.read(filePath).toUTF8()
+    }
+    return ''
+}
+
+function translate(query, completion) {
+    (async () => {
+        const targetLanguage = utils.langMap.get(query.detectTo);
+        const sourceLanguage = utils.langMap.get(query.detectFrom);
+        if (!targetLanguage) {
+            const err = new Error();
+            Object.assign(err, {
+                _type: 'unsupportLanguage',
+                _message: '不支持该语种',
+            });
+            throw err;
+        }
+        const source_lang = sourceLanguage || 'en';
+        const target_lang = targetLanguage || 'zh';
+        const translate_text = query.text || '';
+        if (translate_text !== '') {
+            try {
+                const transResponse = await __translate(source_lang, target_lang, translate_text)
+                if (transResponse.data && transResponse.data.targetText ) {
+                    completion({
+                        result: {
+                            from: query.detectFrom,
+                            to: query.detectTo,
+                            toParagraphs: transResponse.data.targetText.split('\n'),
+                        },
+                    });
+                } else {
+                    $log.error('接口请求错误 transResponse.data ==> ' + JSON.stringify(transResponse.data))
+                    completion({
+                        error: {
+                            type: 'unknown',
+                            message: transResponse.data,
+                            addtion: transResponse.data,
+                        },
+                    });
+                }
+            } catch (e) {
+                $log.error('接口请求错误 ==> ' + JSON.stringify(e))
+                Object.assign(e, {
+                    _type: 'network',
+                    _message: '接口请求错误 - ' + JSON.stringify(e),
+                });
+                throw e;
+            }
+        }
+    })().catch((err) => {
+        completion({
+            error: {
+                type: err._type || 'unknown',
+                message: err._message || '未知错误',
+                addtion: err._addtion,
+            },
+        });
+    });
+}
+
+exports.supportLanguages = supportLanguages;
+exports.translate = translate;
